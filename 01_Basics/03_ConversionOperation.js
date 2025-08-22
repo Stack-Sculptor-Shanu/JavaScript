3d48f50214ae4eae2599a8e1969909e8937ab3fb
Amarjeet Kumar <127886134+Stack-Sculptor-Shanu@users.noreply.github.com>
2025-08-22 19:31:04 +0000

    more on datatypes

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -52,30 +52,68 @@
 // console.log(typeof boolDinner) //boolean
 
 //! Now we'll try to convert other datatypes in string
-let someNumber = 2
-let strNum = String(someNumber)
-let married = true
-let strbool = String(married)
-let myscore = undefined
-let strUd = String(myscore)
-let placement = null
-let strNull = String(placement)
-console.log(someNumber) //2
-console.log(typeof someNumber) //number
-console.log(strNum) //2
-console.log(typeof strNum)// string
-console.log(married) //true
-console.log(typeof married) //boolean
-console.log(strbool) //true
-console.log(typeof strbool) //string
-console.log(myscore) //undefined
-console.log(typeof myscore) //undefined
-console.log(strUd) //undefined
-console.log(typeof strUd) //string
-console.log(placement) //null
-console.log(typeof placement) //object
-console.log(strNull) //null 
-console.log(typeof strNull) //string
+// let someNumber = 2
+// let strNum = String(someNumber)
+// let married = true
+// let strbool = String(married)
+// let myscore = undefined
+// let strUd = String(myscore)
+// let placement = null
+// let strNull = String(placement)
+
+// console.log(someNumber) //2
+// console.log(typeof someNumber) //number
+// console.log(strNum) //2
+// console.log(typeof strNum)// string
+// console.log(married) //true
+// console.log(typeof married) //boolean
+// console.log(strbool) //true
+// console.log(typeof strbool) //string
+// console.log(myscore) //undefined
+// console.log(typeof myscore) //undefined
+// console.log(strUd) //undefined
+// console.log(typeof strUd) //string
+// console.log(placement) //null
+// console.log(typeof placement) //object
+// console.log(strNull) //null 
+// console.log(typeof strNull) //string
 
 
+//*****************Operations********************** */
+let value = 3
+let negValue  = -value
+// console.log(negValue)
 
+// console.log(2+2)
+// console.log(20-2)
+// console.log(20*2)
+// console.log(20**2)
+// console.log(20/2)
+// console.log(20%2)
+
+let str1 = 'shanu'
+let str2 = ' Kumar'
+let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1"+2) //12
+// console.log(1+'2') //12
+// console.log(1+'2' +3) //123
+// console.log(1+2 +'3') //33
+
+// console.log(true) //true
+// console.log(+true) //1
+// console.log(+"") //0
+// console.log(-"") //-0
+
+// let num1, num2, num3
+// num1 = 20
+// num2 = 30
+// num3 = 40
+
+let gameCounter = 100
+gameCounter++ //postfix
+console.log(gameCounter) //101
+++gameCounter //precedence prefix
+console.log(gameCounter) //102
+// for more visit documentation mdn 
