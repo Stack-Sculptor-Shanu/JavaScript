0f9fe60e70a7cebc9ddb27c0462625a69c04b903
Amarjeet Kumar <127886134+Stack-Sculptor-Shanu@users.noreply.github.com>
2025-08-25 19:43:58 +0000

    iife and execution context

---
+++ 03_Basics/04_iife.js
@@ -0,0 +1,17 @@
+//Immediately Invoked functions
+// function tea(){
+//     console.log('DB connected')
+// }
+// tea()
+
+(function tea(){
+    console.log('DB connected')
+})();
+
+//syntax
+// (Function declaration)(FunctionExecution)
+ (()=>{
+    console.log("Hii shanu")
+ })()
+
+ //basically this function is used to remove global scope pollution
